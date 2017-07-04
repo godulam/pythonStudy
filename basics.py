3aefea82f12099a550a47e9a0457dbd4266e1370
godulam <maciek.godula@gmail.com>
2017-07-04 15:27:49 +0200

    new

---
+++ basics.py
@@ -0,0 +1,37 @@
+# use format method to convert
+# age variable to string
+
+age = 20
+name = "Zbigniew"
+
+print("{0} is {1}".format(name, age))
+
+# or it can be done that way
+age += 5
+print(name + " is " + str(age))
+
+# numbers in {} brackets are optional
+age -= 14
+print("{} is {}".format(name, age))
+
+# by default print ends with \n new line
+# use below code to write in one line
+
+print('\n', end='')
+print('a', end=';')
+print('b', end='@')
+print('c')
+
+import math
+# decimal (.) precision of 3 for float '0.333'
+pi = math.pi
+print('\n{0:.3f}'.format(pi))
+
+# fill with underscores (_) with the text centered
+# (^) to 22 width '___hello___'
+print('{0:_^22}'.format('hello'))
+
+# keyword-based 'Zbigniew wrote "Necronicon'
+print('{name} wrote {book}'.format(name='Zbigniew', book='Necronicon'))
+
+# escape 
---
+++ multiplicationTable.py
@@ -0,0 +1,8 @@
+x=1
+y=1
+for y in range (1,11):
+    for x in range (1,11):
+        print(x*y, end=' ')
+    print('')
+    x = x+1
+
