54cc78ed46cd1db50f7b5f5eddbf413f0cd0e1fe
godulam <maciek.godula@gmail.com>
2017-07-05 15:23:19 +0200

    05-07-2017v0.4

--- multiplicationTable.py
+++ multiplicationTable.py
@@ -1,8 +1,16 @@
 x=1
 y=1
+
+matrix = {}
+
 for y in range (1,11):
     for x in range (1,11):
+        matrix[y,x] = x * y
         print(x*y, end=' ')
     print('')
-    x = x+1
+    x += 1
+
+print ()
+print (matrix[5,6])
+print(matrix[2,9])
 
