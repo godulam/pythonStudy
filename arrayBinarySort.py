acf6220d0ab1b7ef3bba72fcbcc87d0313f15010
godulam <maciek.godula@gmail.com>
2017-07-05 14:03:52 +0200

    05-07-2017v0.3

--- arrayBinarySort.py
+++ arrayBinarySort.py
@@ -4,15 +4,18 @@ myArray = sorted(myArray)
 
 arrLen = len(myArray)
 halfLen = arrLen // 2
-print (halfLen)
+
+print (myArray)
 
 searchVal = input('What do you want to find? ')
+
 if (int(searchVal) < myArray[halfLen]):
     for i in range (0, halfLen):
         if (myArray[i]==int(searchVal)):
-            print('found on position {}'.format(i+1))
-# else:
-#     for j in range (halfLen+1, arrLen):
-#         if (myArray[j]==int(searchVal))
-#             print('found on position {}'.format(j+1))
-#
+            print('found on position {}'.format(i))
+elif (int(searchVal) > myArray[halfLen]):
+    for j in range (halfLen, arrLen):
+        if (myArray[j]==int(searchVal)):
+            print('found on position {}'.format(j))
+else:
+    print('found on position {}'.format(halfLen))
