ae4975b8a7c657b05427478b4a249ac38a431193
godulam <maciek.godula@gmail.com>
2017-07-10 15:15:54 +0200

    10-07-2017v0.3

--- calculator.py
+++ calculator.py
@@ -1,18 +1,37 @@
 #!/usr/bin/python3
 
-a = input('type first value: ')
-action = input('what kind of operation you\'d like to perform? (+ - \
-/ *): ')
-b = input('type second value: ')
+from sys import argv
+program, a, action, b = argv
 
+"""
+a = input('type first value: ')
+action = input('what kind of operation you\'d like to perform?\n \
+(+ - / * %[modulo] **[exponent] //[floor division]): ')
+b = input('type second value: ')
+"""
+
+# sum
 if (action=='+' and a.isnumeric() and b.isnumeric()):
     print('The sum is ',float(a)+float(b))
+# sub
 elif (action=='-' and a.isnumeric() and b.isnumeric()):
     print('The sub is ',float(a)-float(b))
+# multikulti
 elif (action=='*' and a.isnumeric() and b.isnumeric()):
     print('The multi is ',float(a)*float(b))
+# div
 elif (action=='/' and a.isnumeric() and b.isnumeric()):
     print('The div is ',float(a)/float(b))
+# modulo
+elif (action=='%' and a.isnumeric() and b.isnumeric()):
+    print('The div is ',float(a)%float(b))
+# exponent
+elif (action=='**' and a.isnumeric() and b.isnumeric()):
+    print('The div is ',float(a)**float(b))
+# floor division
+elif (action=='//' and a.isnumeric() and b.isnumeric()):
+    print('The div is ',float(a)//float(b))
+# if the input is wrong, below message is printed out
 else:
     print('wrong input')
 
