0ba169d8615454d1b239ac1d439d8fb640a76f79
godulam <maciek.godula@gmail.com>
2017-08-06 17:32:05 +0200

    06-08-2017_17:30

---
+++ gabi_show.py
@@ -0,0 +1,4 @@
+k = 5
+
+if k == 5:
+    print ("Hello")
---
+++ simplestClass.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+class Person:
+
+    def __init__(self, name):
+        self.name = name
+
+    def say_hi(self):
+        print("Hi! My name is ", self.name)
+
+p=Person('Kokos')
+
+p.say_hi()
